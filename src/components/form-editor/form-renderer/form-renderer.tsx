1741f7907ae77bb2aeabb43fbf784a4c61df0826
kumuditha-udayanga <kumudithaudaya@gmail.com>
2022-09-20 00:12:32 +0530

    Manage patient uuid through configuration system

--- src/components/form-editor/form-renderer/form-renderer.tsx
+++ src/components/form-editor/form-renderer/form-renderer.tsx
@@ -2,8 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { OHRIForm } from "@ohri/openmrs-ohri-form-engine-lib";
 import { OHRIFormSchema } from "@ohri/openmrs-ohri-form-engine-lib/src/api/types";
 import { SchemaContext } from "../../../context/context";
+import { useConfig } from "@openmrs/esm-framework";
 
 const FormRenderer: React.FC = () => {
+  const { patientUuidConfig } = useConfig();
   const defaultSchema: OHRIFormSchema = {
     name: "",
     pages: [],
@@ -12,7 +14,7 @@ const FormRenderer: React.FC = () => {
     encounterType: "",
     referencedForms: [],
   };
-  const patientUUID = "b280078a-c0ce-443b-9997-3c66c63ec2f8";
+  const patientUUID = patientUuidConfig;
   const [currentFormMode, setCurrentFormMode] = useState<any>("enter");
   const { schema, setSchema } = useContext(SchemaContext);
   const [renderFormSchema, setRenderFormSchema] =
