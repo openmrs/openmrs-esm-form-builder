ec347cac78feb3d48d09b41b62c054e944d3c3c1
Dennis Kigen <kigen.work@gmail.com>
2022-11-18 23:41:50 +0300

    (feat) Landing page UI tweaks

--- src/components/dashboard/dashboard.component.tsx
+++ src/components/dashboard/dashboard.component.tsx
@@ -1,92 +1,90 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useTranslation } from "react-i18next";
 import {
+  Button,
   DataTable,
+  DataTableSkeleton,
+  Layer,
   Table,
-  TableHead,
-  TableRow,
-  TableHeader,
   TableBody,
   TableCell,
   TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
   TableToolbar,
-  TableToolbarSearch,
-  TableToolbarAction,
-  TableToolbarMenu,
   TableToolbarContent,
-  Button,
+  TableToolbarSearch,
   Tag,
-  Link,
+  Tile,
 } from "@carbon/react";
-import { navigate } from "@openmrs/esm-framework";
-import { Download, Edit, DocumentImport } from "@carbon/react/icons";
-import { useTranslation } from "react-i18next";
+import { Add, DocumentImport, Download, Edit } from "@carbon/react/icons";
+import { navigate, useLayoutType } from "@openmrs/esm-framework";
+
 import { usePOCForms } from "../../api/usePOCForms";
-import styles from "./dashboard.css";
+import EmptyState from "../empty-state/empty-state.component";
+import ErrorState from "../error-state/error-state.component";
+import styles from "./dashboard.scss";
+
+function CustomTag({ condition }) {
+  const { t } = useTranslation();
+
+  return condition ? (
+    <Tag type="green" size="md" title="Clear Filter">
+      {t("yes", "Yes")}
+    </Tag>
+  ) : (
+    <Tag type="red" size="md" title="Clear Filter">
+      {t("no", "No")}
+    </Tag>
+  );
+}
+
+function ActionButtons({ form }) {
+  const { t } = useTranslation();
+
+  return form?.resources?.length == 0 || !form?.resources?.[0] ? (
+    <Button
+      className={styles.importButton}
+      renderIcon={DocumentImport}
+      onClick={() => navigate({ to: `form-builder/edit/${form.uuid}` })}
+      kind={"ghost"}
+      iconDescription={t("import", "Import")}
+      hasIconOnly
+    />
+  ) : (
+    <>
+      <Button
+        className={styles.editButton}
+        enterDelayMs={300}
+        renderIcon={Edit}
+        onClick={() =>
+          navigate({
+            to: `${window.spaBase}/form-builder/edit/${form.uuid}`,
+          })
+        }
+        kind={"ghost"}
+        iconDescription={t("edit", "Edit")}
+        hasIconOnly
+      />
+      <Button
+        className={styles.downloadButton}
+        enterDelayMs={300}
+        renderIcon={Download}
+        kind={"ghost"}
+        iconDescription={t("download", "Download")}
+        hasIconOnly
+      />
+    </>
+  );
+}
 
 const Dashboard: React.FC = () => {
   const { t } = useTranslation();
-  const { forms } = usePOCForms();
-  const rows = [];
-  forms?.map((form, key) =>
-    rows.push({
-      id: `${key}`,
-      name: form.name,
-      version: form.version,
-      published: form.published ? (
-        <Tag type="green" size="sm" title="Clear Filter">
-          {t("yes", "Yes")}
-        </Tag>
-      ) : (
-        <Tag type="red" size="sm" title="Clear Filter">
-          {t("no", "No")}
-        </Tag>
-      ),
-      retired: form.retired ? (
-        <Tag type="red" size="sm" title="Clear Filter">
-          {t("yes", "Yes")}
-        </Tag>
-      ) : (
-        <Tag type="green" size="sm" title="Clear Filter">
-          {t("no", "No")}
-        </Tag>
-      ),
-      actions:
-        form.resources.length == 0 || !form.resources[0] ? (
-          <Button
-            className={styles.importButton}
-            renderIcon={DocumentImport}
-            onClick={() => navigate({ to: `form-builder/edit/${form.uuid}` })}
-            kind={"ghost"}
-            iconDescription={t("import", "Import")}
-            hasIconOnly
-          />
-        ) : (
-          <>
-            <Button
-              className={styles.editButton}
-              renderIcon={Edit}
-              onClick={() =>
-                navigate({
-                  to: `${window.spaBase}/form-builder/edit/${form.uuid}`,
-                })
-              }
-              kind={"ghost"}
-              iconDescription={t("editForm", "Edit Form")}
-              hasIconOnly
-            />
-            <Button
-              className={styles.downloadButton}
-              renderIcon={Download}
-              kind={"ghost"}
-              iconDescription={t("download", "Download")}
-              hasIconOnly
-            />
-          </>
-        ),
-    })
-  );
+  const { error, forms, isLoading, isValidating } = usePOCForms();
+  const isTablet = useLayoutType() === "tablet";
 
-  const headers = [
+  const tableHeaders = [
     {
       header: t("name", "Name"),
       key: "name",
@@ -104,92 +102,135 @@ const Dashboard: React.FC = () => {
       key: "retired",
     },
     {
-      header: t("actions", "Actions"),
+      header: t("schemaActions", "Schema actions"),
       key: "actions",
     },
   ];
 
+  const tableRows = useMemo(
+    () =>
+      forms?.map((form) => ({
+        ...form,
+        id: form.uuid,
+        published: <CustomTag condition={form.published} />,
+        retired: <CustomTag condition={form.retired} />,
+        actions: <ActionButtons form={form} />,
+      })),
+    [forms]
+  );
+
+  if (error) {
+    return <ErrorState error={error} />;
+  }
+
+  if (isLoading) {
+    return <DataTableSkeleton role="progressbar" />;
+  }
+
+  if (forms.length === 0) {
+    return (
+      <>
+        <h3 className={styles.heading}>{t("formBuilder", "Form Builder")}</h3>
+        <EmptyState />
+      </>
+    );
+  }
+
   return (
     <div>
-      <DataTable rows={rows} headers={headers} className={styles.wrapContainer}>
-        {({
-          rows,
-          headers,
-          getTableProps,
-          getHeaderProps,
-          getRowProps,
-          getToolbarProps,
-          onInputChange,
-          getTableContainerProps,
-        }) => (
-          <TableContainer
-            title={t("formBuilder", "Form builder")}
-            {...getTableContainerProps()}
+      <h3 className={styles.heading}>{t("formBuilder", "Form Builder")}</h3>
+      <div className={styles.container}>
+        <div className={styles.buttonContainer}>
+          <Button
+            size="md"
+            kind="secondary"
+            className={styles.createFormButton}
+            renderIcon={(props) => <Add size={16} {...props} />}
+            onClick={() =>
+              navigate({
+                to: `${window.spaBase}/form-builder/edit/new`,
+              })
+            }
+            iconDescription={t("createNewForm", "Create new form")}
           >
-            <div className={styles.toolbar}>
-              <TableToolbar
+            {t("createNewForm", "Create new form")}
+          </Button>
+        </div>
+        <DataTable
+          rows={tableRows}
+          headers={tableHeaders}
+          size={isTablet ? "lg" : "xs"}
+          useZebraStyles
+        >
+          {({
+            rows,
+            headers,
+            getTableProps,
+            getHeaderProps,
+            getRowProps,
+            getToolbarProps,
+            onInputChange,
+          }) => (
+            <>
+              <TableContainer
                 {...getToolbarProps()}
-                aria-label="data table toolbar"
+                className={styles.tableContainer}
               >
-                <TableToolbarContent>
-                  <TableToolbarSearch onChange={onInputChange} />
-                  <TableToolbarMenu>
-                    <TableToolbarAction onClick={() => {}}>
-                      POC
-                    </TableToolbarAction>
-                  </TableToolbarMenu>
-                  <Button
-                    onClick={() =>
-                      navigate({
-                        to: `${window.spaBase}/form-builder/edit/new`,
-                      })
-                    }
-                  >
-                    {t("createNew", "Create New")}
-                  </Button>
-                </TableToolbarContent>
-              </TableToolbar>
-            </div>
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map((header) => (
-                    <TableHeader {...getHeaderProps({ header })}>
-                      {header.header}
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.length > 0 ? (
-                  rows.map((row) => (
-                    <TableRow {...getRowProps({ row })}>
-                      {row.cells.map((cell) => (
-                        <TableCell key={cell.id}>{cell.value}</TableCell>
+                <TableToolbar>
+                  <TableToolbarContent>
+                    <Layer>
+                      <TableToolbarSearch
+                        className={styles.search}
+                        expanded
+                        onChange={onInputChange}
+                        placeholder={t("searchThisList", "Search this list")}
+                        size={isTablet ? "lg" : "sm"}
+                      />
+                    </Layer>
+                  </TableToolbarContent>
+                </TableToolbar>
+                <Table {...getTableProps()} className={styles.table}>
+                  <TableHead>
+                    <TableRow>
+                      {headers.map((header) => (
+                        <TableHeader {...getHeaderProps({ header })}>
+                          {header.header}
+                        </TableHeader>
                       ))}
                     </TableRow>
-                  ))
-                ) : (
-                  <TableRow className={styles.emptyContainer}>
-                    <TableCell>
-                      There are No forms found.
-                      <Link
-                        onClick={() =>
-                          navigate({
-                            to: `${window.spaBase}/form-builder/edit/new`,
-                          })
-                        }
-                      >
-                        {t("createNew", "Create New")}
-                      </Link>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-      </DataTable>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow {...getRowProps({ row })}>
+                        {row.cells.map((cell) => (
+                          <TableCell key={cell.id}>{cell.value}</TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              {rows.length === 0 ? (
+                <div className={styles.tileContainer}>
+                  <Tile className={styles.tile}>
+                    <div className={styles.tileContent}>
+                      <p className={styles.content}>
+                        {t(
+                          "noMatchingFormsToDisplay",
+                          "No matching forms to display"
+                        )}
+                      </p>
+                      <p className={styles.helper}>
+                        {t("checkFilters", "Check the filters above")}
+                      </p>
+                    </div>
+                  </Tile>
+                </div>
+              ) : null}
+            </>
+          )}
+        </DataTable>
+      </div>
     </div>
   );
 };
---
+++ src/components/error-state/error-state.component.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Layer, Tile } from "@carbon/react";
+import { useTranslation } from "react-i18next";
+import { useLayoutType } from "@openmrs/esm-framework";
+import styles from "./error-state.scss";
+
+interface ErrorStateProps {
+  error: Error;
+}
+
+const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
+  const { t } = useTranslation();
+  const isTablet = useLayoutType() === "tablet";
+
+  return (
+    <Layer>
+      <Tile className={styles.tile}>
+        <div
+          className={isTablet ? styles.tabletHeading : styles.desktopHeading}
+        >
+          <h4>{t("forms", "Forms")}</h4>
+        </div>
+        <p className={styles.errorMessage}>
+          {t("error", "Error")}: {error?.message}
+        </p>
+        <p className={styles.errorCopy}>
+          {t(
+            "errorCopy",
+            "Sorry, there was a problem displaying this information. You can try to reload this page, or contact the site administrator and quote the error code above."
+          )}
+        </p>
+      </Tile>
+    </Layer>
+  );
+};
+
+export default ErrorState;
