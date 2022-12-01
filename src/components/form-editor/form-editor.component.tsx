61c336247afd3e8856f6f9e9568655525a9f0876
Dennis Kigen <kigen.work@gmail.com>
2022-12-01 15:27:04 +0300

    (feat) Improvements to the form publishing workflow

--- src/components/form-editor/form-editor.component.tsx
+++ src/components/form-editor/form-editor.component.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import {
   Column,
+  ComposedModal,
+  InlineLoading,
   InlineNotification,
   Grid,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
   Tabs,
   Tab,
   TabList,
@@ -11,14 +16,19 @@ import {
   Button,
 } from "@carbon/react";
 import { useTranslation } from "react-i18next";
+import { useSWRConfig } from "swr";
 import SchemaEditorComponent from "../schema-editor/schema-editor.component";
 import styles from "./form-editor.scss";
 import { useParams } from "react-router-dom";
 import SaveForm from "../modals/save-form.component";
-import { showToast, ExtensionSlot } from "@openmrs/esm-framework";
+import {
+  showToast,
+  ExtensionSlot,
+  showNotification,
+} from "@openmrs/esm-framework";
 import { useClobdata } from "../../hooks/useClobdata";
 import { useForm } from "../../hooks/useForm";
-import { publish, unpublish } from "../../forms.resource";
+import { publishForm, unpublishForm } from "../../forms.resource";
 import FormRenderer from "../form-renderer/form-renderer.component";
 import ElementEditor from "../element-editor/element-editor.component";
 import { Schema } from "../../types";
@@ -45,53 +55,90 @@ const Error = ({ error, title }) => {
 
 const FormEditor: React.FC = () => {
   const { t } = useTranslation();
-  const [schema, setSchema] = useState<Schema>(undefined);
   const { formUuid } = useParams<Route>();
+  const [schema, setSchema] = useState<Schema>(undefined);
+  const [isPublishing, setIsPublishing] = useState(false);
+  const [isUnpublishing, setIsUnpublishing] = useState(false);
+  const [showUnpublishModal, setShowUnpublishModal] = useState(false);
   const { form, formError, isLoadingForm } = useForm(formUuid);
   const { clobdata, clobdataError, isLoadingClobdata } = useClobdata(form);
+  const { cache, mutate }: { cache: any; mutate: Function } = useSWRConfig();
+
+  useEffect(() => {
+    if (clobdata) {
+      setSchema(clobdata);
+    }
+  }, [clobdata, setSchema]);
 
   const updateSchema = (updatedSchema) => {
     setSchema(updatedSchema);
   };
 
-  const handlePublishState = async (option) => {
-    if (option == "publish") {
-      try {
-        await publish(form?.uuid);
-        showToast({
-          title: t("success", "Success!"),
-          kind: "success",
-          critical: true,
-          description: t("publishedSuccess", "Form Published"),
-        });
-      } catch (error) {
-        showToast({
-          title: t("error", "Error"),
-          kind: "error",
-          critical: true,
-          description: error?.message,
-        });
-      }
-    } else if (option == "unpublish") {
-      try {
-        await unpublish(form?.uuid);
-        showToast({
-          title: t("success", "Success!"),
-          kind: "success",
-          critical: true,
-          description: t("unpublishedSuccess", "Form Unpublished"),
-        });
-      } catch (error) {
-        showToast({
-          title: t("error", "Error"),
-          kind: "error",
-          critical: true,
-          description: error?.message,
-        });
-      }
-    }
-    window.location.reload();
+  const revalidate = () => {
+    const apiUrlPattern = new RegExp("\\/ws\\/rest\\/v1\\/form");
+
+    // Find matching keys from SWR's cache and broadcast a revalidation message to their pre-bound SWR hooks
+    Array.from(cache.keys())
+      .filter((url: string) => apiUrlPattern.test(url))
+      .forEach((url: string) => mutate(url));
   };
+
+  const launchUnpublishModal = () => {
+    setShowUnpublishModal(true);
+  };
+
+  async function handlePublish() {
+    setIsPublishing(true);
+    try {
+      await publishForm(form.uuid);
+
+      showToast({
+        title: t("formPublished", "Form published"),
+        kind: "success",
+        critical: true,
+        description:
+          `${form.name} ` +
+          t("formPublishedSuccessfully", "form was published successfully"),
+      });
+
+      revalidate();
+    } catch (error) {
+      showNotification({
+        title: t("errorPublishingForm", "Error publishing form"),
+        kind: "error",
+        critical: true,
+        description: error?.message,
+      });
+    }
+    setIsPublishing(false);
+  }
+
+  async function handleUnpublish() {
+    setIsUnpublishing(true);
+    try {
+      await unpublishForm(form.uuid);
+
+      showToast({
+        title: t("formUnpublished", "Form unpublished"),
+        kind: "success",
+        critical: true,
+        description:
+          `${form.name} ` +
+          t("formUnpublishedSuccessfully", "form was unpublished successfully"),
+      });
+
+      revalidate();
+    } catch (error) {
+      showNotification({
+        title: t("errorUnpublishingForm", "Error unpublishing form"),
+        kind: "error",
+        critical: true,
+        description: error?.message,
+      });
+    }
+    setIsUnpublishing(false);
+    setShowUnpublishModal(false);
+  }
 
   useEffect(() => {
     if (!isLoadingClobdata) {
@@ -108,27 +155,78 @@ const FormEditor: React.FC = () => {
         <div className={styles.actionsContainer}>
           <SaveForm form={form} schema={schema} />
 
-          {form?.published == true ? (
-            <Button
-              className={styles.optionButtons}
-              onClick={() => handlePublishState("unpublish")}
-              kind="primary"
-            >
-              {t("unpublishForm", "Unpublish form")}
-            </Button>
-          ) : form?.published == false ? (
-            <Button
-              className={styles.optionButtons}
-              onClick={() => handlePublishState("publish")}
-              kind="primary"
-            >
-              {t("publishForm", "Publish form")}
-            </Button>
-          ) : (
-            <Button className={styles.optionButtons} kind="primary" disabled>
-              {t("publishForm", "Publish form")}
-            </Button>
-          )}
+          <>
+            {form && !form.published ? (
+              <Button
+                kind="secondary"
+                onClick={handlePublish}
+                disabled={isPublishing}
+              >
+                {isPublishing && !form?.published ? (
+                  <InlineLoading
+                    className={styles.spinner}
+                    description={t("publishing", "Publishing") + "..."}
+                  />
+                ) : (
+                  <span>{t("publishForm", "Publish form")}</span>
+                )}
+              </Button>
+            ) : null}
+            {form && form.published ? (
+              <Button
+                kind="danger"
+                onClick={launchUnpublishModal}
+                disabled={isUnpublishing}
+              >
+                {t("unpublishForm", "Unpublish form")}
+              </Button>
+            ) : null}
+            {showUnpublishModal ? (
+              <ComposedModal
+                open={true}
+                onClose={() => setShowUnpublishModal(false)}
+              >
+                <ModalHeader
+                  title={t(
+                    "unpublishConfirmation",
+                    "Are you sure you want to unpublish this form?"
+                  )}
+                ></ModalHeader>
+                <ModalBody>
+                  <p>
+                    {t(
+                      "unpublishExplainerText",
+                      "Unpublishing a form means you can no longer access it from your frontend. Unpublishing forms does not delete their associated schemas, it only affects whether or not you can access them in your frontend."
+                    )}
+                  </p>
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    kind="secondary"
+                    onClick={() => setShowUnpublishModal(false)}
+                  >
+                    {t("cancel", "Cancel")}
+                  </Button>
+                  <Button
+                    disabled={isUnpublishing}
+                    kind={isUnpublishing ? "secondary" : "danger"}
+                    onClick={handleUnpublish}
+                  >
+                    {isUnpublishing ? (
+                      <InlineLoading
+                        className={styles.spinner}
+                        description={t("unpublishing", "Unpublishing") + "..."}
+                      />
+                    ) : (
+                      <span>{t("unpublishForm", "Unpublish form")}</span>
+                    )}
+                  </Button>
+                </ModalFooter>
+              </ComposedModal>
+            ) : (
+              false
+            )}
+          </>
         </div>
         <Grid className={styles.grid}>
           <Column lg={8} md={8} className={styles.column}>
