6f29fe3a3d6b6a0d561920f228cb19706e2e5b4c
Dennis Kigen <kigen.work@gmail.com>
2022-12-01 10:41:44 +0300

    (refactor) Refactor useForm hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -2,17 +2,17 @@ import useSWRImmutable from "swr/immutable";
 import { openmrsFetch } from "@openmrs/esm-framework";
 import { Form } from "../types";
 
-export const useFormMetadata = (uuid: string) => {
+export const useForm = (uuid: string) => {
   const url = `/ws/rest/v1/form/${uuid}?v=full`;
 
   const { data, error } = useSWRImmutable<{ data: Form }, Error>(
-    uuid === "new" ? null : url,
+    uuid ? url : null,
     openmrsFetch
   );
 
   return {
-    metadata: data?.data,
-    error: error,
-    isLoading: !data && !error,
+    form: data?.data,
+    formError: error,
+    isLoadingForm: !data && !error,
   };
 };
