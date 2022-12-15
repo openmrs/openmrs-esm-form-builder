b924495d6e149668e429b6939af0c163d736599d
Dennis Kigen <kigen.work@gmail.com>
2022-12-15 15:45:28 +0300

    (fix) Remove POC prefix requirement and modify useForm hook (#40)

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,11 +1,11 @@
-import useSWRImmutable from "swr/immutable";
+import useSWR from "swr/immutable";
 import { openmrsFetch } from "@openmrs/esm-framework";
 import { Form } from "../types";
 
 export const useForm = (uuid: string) => {
   const FORM_URL = `/ws/rest/v1/form/${uuid}?v=full`;
 
-  const { data, error } = useSWRImmutable<{ data: Form }, Error>(
+  const { data, error } = useSWR<{ data: Form }, Error>(
     uuid ? FORM_URL : null,
     openmrsFetch
   );
--- src/hooks/useForms.ts
+++ src/hooks/useForms.ts
@@ -2,9 +2,9 @@ import useSWR from "swr";
 import { openmrsFetch } from "@openmrs/esm-framework";
 import { Form } from "../types";
 
-export function usePocForms() {
+export function useForms() {
   const FORMS_URL =
-    "/ws/rest/v1/form?q=POC&v=custom:(uuid,name,encounterType:(uuid,name),version,published,retired,resources:(uuid,name,dataType,valueReference))";
+    "/ws/rest/v1/form?v=custom:(uuid,name,encounterType:(uuid,name),version,published,retired,resources:(uuid,name,dataType,valueReference))";
 
   const { data, error, isValidating } = useSWR<
     { data: { results: Array<Form> } },
