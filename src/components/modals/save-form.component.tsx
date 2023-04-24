f2953ebcd07d0e6398640871b2719a997065f831
Donald Kibet <chelashawdonald@yahoo.com>
2023-04-24 14:30:26 +0300

    (fix) ensure users do not input wrong version numbers

--- src/components/modals/save-form.component.tsx
+++ src/components/modals/save-form.component.tsx
@@ -57,6 +57,13 @@ const SaveForm: React.FC<SaveFormModalProps> = ({ form, schema, onMutate }) => {
   const [openConfirmSaveModal, setOpenConfirmSaveModal] = useState(false);
   const [saveState, setSaveState] = useState("");
   const [isSavingForm, setIsSavingForm] = useState(false);
+  const [hasValidVersion, setHasValidVersion] = useState(false);
+
+  const isVersionValid = (version: string) => {
+    if (version === "") return setHasValidVersion(false);
+    const versionRegex = new RegExp(/^[0-9]/);
+    setHasValidVersion(Boolean(!versionRegex.test(version)));
+  };
 
   const openModal = useCallback((option) => {
     if (option === "newVersion") {
@@ -250,6 +257,7 @@ const SaveForm: React.FC<SaveFormModalProps> = ({ form, schema, onMutate }) => {
       ) : null}
 
       <ComposedModal
+        preventCloseOnClickOutside
         open={openSaveFormModal}
         onClose={() => setOpenSaveFormModal(false)}
       >
@@ -287,6 +295,12 @@ const SaveForm: React.FC<SaveFormModalProps> = ({ form, schema, onMutate }) => {
                   defaultValue={saveState === "update" ? form?.version : ""}
                   placeholder="e.g. 1.0"
                   required
+                  onChange={(event) => isVersionValid(event.target.value)}
+                  invalid={hasValidVersion}
+                  invalidText={t(
+                    "versionErrorMessages",
+                    "Version cannot start with letters ensure valid version e.g 1.0"
+                  )}
                 />
                 <Select
                   id="encounterType"
@@ -337,7 +351,7 @@ const SaveForm: React.FC<SaveFormModalProps> = ({ form, schema, onMutate }) => {
               {t("close", "Close")}
             </Button>
             <Button
-              disabled={isSavingForm}
+              disabled={isSavingForm || hasValidVersion}
               className={styles.spinner}
               type={"submit"}
               kind={"primary"}
