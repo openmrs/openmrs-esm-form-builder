f45f3b88c2a6d014ea0e404a8db77e62809da37f
anjula-sack <anjulashanaka@gmail.com>
2023-05-09 18:21:39 +0530

    Move the locators to the pom

--- e2e/specs/customSchemaForm.spec.ts
+++ e2e/specs/customSchemaForm.spec.ts
@@ -12,24 +12,14 @@ test("Should be able to create a form using custom schema", async ({
   const formName = `test form ${Math.floor(Math.random() * 10000)}`;
 
   await formBuilderPage.gotoFormBuilder();
-  await formBuilderPage.clickCreateNewFormButton();
+  await formBuilderPage.createNewFormButton().click();
 
   // Inputs the custom schema and render changes
-  await page.locator(".ace_text-input").fill(JSON.stringify(customSchema));
-  await page.getByRole("button", { name: "Render changes" }).click();
+  await formBuilderPage.schemaInput().fill(JSON.stringify(customSchema));
+  await formBuilderPage.renderChangesButton().click();
 
   // Save the form
-  await page.getByRole("button", { name: "Save Form" }).click();
-  await page.getByLabel("Form name").click();
-  await page.getByLabel("Form name").fill(formName);
-  await page.getByLabel("Version").click();
-  await page.getByLabel("Version").fill("1.0");
-  await page.getByLabel("Description").click();
-  await page.getByLabel("Description").fill("this is test form description");
-  await page
-    .getByRole("combobox", { name: "Encounter Type" })
-    .selectOption("Admission");
-  await page.getByRole("dialog").getByRole("button", { name: "Save" }).click();
+  await formBuilderPage.saveForm(formName);
 
   // Checks whether the user has been redirected to the edit page
   const editFormPageURLRegex = new RegExp("/edit/");
