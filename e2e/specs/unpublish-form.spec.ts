5ca6640fdf15d1b3a18b74159b1f9741cec79dc0
piumal1999 <piumalrathnayake@hotmail.com>
2023-06-26 00:05:25 +0530

    Change selector of edit button

--- e2e/specs/unpublish-form.spec.ts
+++ e2e/specs/unpublish-form.spec.ts
@@ -28,7 +28,7 @@ test("Unpublish a form", async ({ page }) => {
   });
 
   await test.step("And I click on a form I need to unpublish", async () => {
-    await page.getByRole('row', { name: form.name }).getByRole('button').first().click();
+    await page.getByRole('row', { name: form.name }).getByLabel('Edit Schema').click();
   });
 
   await test.step("Then I click on the unpublish form button and confirms the unpublication", async () => {
