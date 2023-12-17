39da9b4159d69f60729e2b118ed246de9df8dcc2
Dennis Kigen <kigen.work@gmail.com>
2023-12-17 23:55:41 +0300

    Update svg illustration

--- src/components/header/illo.component.tsx
+++ src/components/header/illo.component.tsx
@@ -5,16 +5,17 @@ const Illustration: React.FC = () => {
     <svg
       height="64"
       width="64"
-      clipRule="evenodd"
-      fillRule="evenodd"
-      strokeLinejoin="round"
       viewBox="0 0 32 32"
       xmlns="http://www.w3.org/2000/svg"
       xmlSpace="preserve"
+      fill-rule="evenodd"
+      clipRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
     >
       <path
-        fill="#d2e5e5"
         d="M27 31.36H8a.36.36 0 0 1-.36-.36v-1.64H6a.36.36 0 0 1-.36-.36v-1.64H5a.36.36 0 0 1-.36-.36V3A.36.36 0 0 1 5 2.64h4.64V2a.36.36 0 0 1 .36-.36h1.64V1A.36.36 0 0 1 12 .64h4a.36.36 0 0 1 .36.36v.64H18a.36.36 0 0 1 .36.36v.64H23a.36.36 0 0 1 .36.36v1.64H25a.36.36 0 0 1 .36.36v1.64H27a.36.36 0 0 1 .36.36v24a.36.36 0 0 1-.36.36Z"
+        fill="#d2e5e5"
       />
       <path d="M8.36 30.64h18.28V7.36h-1.28V29a.36.36 0 0 1-.36.36H8.36v1.28Z" fill="#8abab8" />
       <path
