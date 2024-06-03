7876196ced6f727fff629ea711941ed379f4159c
senthil-k8s <senthilathiban0@gmail.com>
2024-06-03 12:41:52 +0530

    (chore): optimize the global state rules

--- src/components/provider/rule-provider.tsx
+++ src/components/provider/rule-provider.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useState } from 'react';
 import { RuleContext } from '../../hooks/useFormRule';
 import { type formRule } from '../rule-builder/rule-builder.component';
 
 export const RuleProvider = ({ children }: { children: React.ReactNode }) => {
   const [rules, setRules] = useState<Array<formRule>>();
+  const cachedRules = useMemo(() => rules, [rules]);
+  const updateRules = useCallback((newRules: Array<formRule>) => setRules(newRules), [setRules]);
 
-  return <RuleContext.Provider value={{ rules, setRules }}>{children}</RuleContext.Provider>;
+  return <RuleContext.Provider value={{ rules: cachedRules, setRules: updateRules }}>{children}</RuleContext.Provider>;
 };
