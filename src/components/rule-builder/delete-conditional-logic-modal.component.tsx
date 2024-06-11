f80dda555289b2ef1ccd731213fbdd4c724d7c54
senthil-k8s <senthilathiban0@gmail.com>
2024-06-11 14:19:33 +0530

    (feat): improve conditional logic delete modal

--- src/components/rule-builder/delete-conditional-logic-modal.component.tsx
+++ src/components/rule-builder/delete-conditional-logic-modal.component.tsx
@@ -1,51 +1,52 @@
-import { ModalHeader } from '@carbon/react';
+import { ModalHeader, ModalBody, ModalFooter, Button } from '@carbon/react';
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ModalBody } from '@carbon/react';
-import { ModalFooter } from '@carbon/react';
-import { Button } from '@carbon/react';
 import { type Action, type Condition, type formRule } from './rule-builder.component';
 import { v4 as uuid } from 'uuid';
 import { showSnackbar } from '@openmrs/esm-framework';
 
 interface DeleteConditionalLogicModalProps {
   closeModal: () => void;
-  questionId: string;
+  questionId?: string;
   questionLabel: string;
-  handleAddLogic: (fieldId: string) => void;
-  setConditions: React.Dispatch<React.SetStateAction<Array<Condition>>>;
-  setActions: React.Dispatch<React.SetStateAction<Array<Action>>>;
-  setCurrentRule: React.Dispatch<React.SetStateAction<formRule>>;
+  ruleId?: string;
+  currentRule?: formRule;
+  rules?: Array<formRule>;
+  handleAddLogic?: (fieldId: string) => void;
+  setConditions?: React.Dispatch<React.SetStateAction<Array<Condition>>>;
+  setActions?: React.Dispatch<React.SetStateAction<Array<Action>>>;
+  setCurrentRule?: React.Dispatch<React.SetStateAction<formRule>>;
   setRules: React.Dispatch<React.SetStateAction<Array<formRule>>>;
+  deleteAll?: boolean; // Flag to determine if all conditional logic should be deleted
 }
 
-const DeleteConditionalLogic = ({
+const DeleteConditionalLogicModal = ({
   closeModal,
-  questionLabel,
   questionId,
+  questionLabel,
+  ruleId,
   handleAddLogic,
   setConditions,
   setActions,
   setCurrentRule,
   setRules,
+  rules,
+  deleteAll = false,
 }: DeleteConditionalLogicModalProps) => {
   const { t } = useTranslation();
-  const deleteConditionalLogic = useCallback(() => {
+
+  const deleteAllConditionalLogic = useCallback(() => {
     try {
-      handleAddLogic(questionId);
-      setConditions([]);
-      setActions([]);
-      setCurrentRule({ id: uuid(), question: questionId });
-      setRules((prevRule: Array<formRule>) => {
-        return prevRule.filter((rule) => rule.question !== questionId);
-      });
+      if (handleAddLogic && questionId) handleAddLogic(questionId);
+      if (setConditions) setConditions([]);
+      if (setActions) setActions([]);
+      if (setCurrentRule) setCurrentRule({ id: uuid(), question: questionId });
+      setRules((prevRule: Array<formRule>) => prevRule.filter((rule) => rule.question !== questionId));
       showSnackbar({
         title: t(
           'conditionalLogicDeletedMessage',
           'Conditional logic for the question "{{- questionLabel}}" has been deleted',
-          {
-            questionLabel: questionLabel,
-          },
+          { questionLabel: questionLabel },
         ),
       });
     } catch (error) {
@@ -60,18 +61,64 @@ const DeleteConditionalLogic = ({
       closeModal();
     }
   }, [closeModal, questionLabel, handleAddLogic, questionId, setActions, setConditions, setCurrentRule, setRules, t]);
+
+  const deleteSpecificConditionalLogic = useCallback(() => {
+    try {
+      setRules((prevRule: Array<formRule>) => {
+        const newRule = [...prevRule];
+        const elementIndex = rules?.findIndex((e) => e.id === ruleId);
+        if (elementIndex !== -1) {
+          newRule.splice(elementIndex, 1);
+        }
+        return newRule;
+      });
+
+      showSnackbar({
+        title: t(
+          'targetedConditionalLogicDeletedMessage',
+          'The specific conditional logic associated with "{{- questionLabel}}" has been successfully deleted.',
+          { questionLabel: questionLabel },
+        ),
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        showSnackbar({
+          title: t('errorDeletingConditionalLogic', 'Error deleting conditional logic'),
+          kind: 'error',
+          subtitle: error?.message,
+        });
+      }
+    } finally {
+      closeModal();
+    }
+  }, [closeModal, rules, setRules, t, ruleId, questionLabel]);
+
+  const deleteLogic = deleteAll ? deleteAllConditionalLogic : deleteSpecificConditionalLogic;
+
   return (
     <div>
       <ModalHeader
         closeModal={closeModal}
-        title={t('deleteConditionalLogicConfirmation', 'Are you sure you want to delete this logic ?')}
+        title={
+          deleteAll
+            ? t('deleteConditionalLogicConfirmation', 'Are you sure you want to delete this logic?')
+            : t(
+                'deleteTargetedConditionalLogicConfirmation',
+                'Are you sure you want to delete the selected conditional logic?',
+              )
+        }
       />
       <ModalBody>
         <p>
-          {t(
-            'deleteConditionalLogicExplainerText',
-            'Deleting this logic will delete all the conditional logics associated with it. This action cannot be undone.',
-          )}
+          {deleteAll
+            ? t(
+                'deleteConditionalLogicExplainerText',
+                'Deleting this logic will delete all the conditional logics associated with it. This action cannot be undone.',
+              )
+            : t(
+                'deleteTargetedConditionalLogicExplainerText',
+                'Deleting this selected conditional logic will permanently delete it from the field. This action cannot be undone.',
+              )}
         </p>
       </ModalBody>
       <ModalFooter>
@@ -81,14 +128,18 @@ const DeleteConditionalLogic = ({
         <Button
           kind="danger"
           onClick={() => {
-            deleteConditionalLogic();
+            deleteLogic();
           }}
         >
-          <span>{t('deleteConditionalLogic', 'Delete conditional logic')}</span>
+          <span>
+            {deleteAll
+              ? t('deleteConditionalLogic', 'Delete conditional logic')
+              : t('confirmDeleteSpecificConditionalLogic', 'Delete selected conditional logic?')}
+          </span>
         </Button>
       </ModalFooter>
     </div>
   );
 };
 
-export default DeleteConditionalLogic;
+export default DeleteConditionalLogicModal;
