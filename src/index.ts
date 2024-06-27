30fe732d6d83a3deb19c5b52b7609aa4c42e6b98
Senthil Athiban <senthilathiban0@gmail.com>
2024-06-27 11:15:15 +0530

    (fix): rename modal file

--- src/index.ts
+++ src/index.ts
@@ -68,12 +68,12 @@ export const deleteFormModal = getAsyncLifecycle(
 );
 
 export const deleteConditionalLogicModal = getAsyncLifecycle(
-  () => import('./components/rule-builder/delete-conditional-logic-modal.component'),
+  () => import('./components/rule-builder/delete-conditional-logic.modal'),
   options,
 );
 
 export const deleteConditionsOrActionsModal = getAsyncLifecycle(
-  () => import('./components/rule-builder/delete-conditions-or-actions-modal.component'),
+  () => import('./components/rule-builder/delete-conditions-or-actions.modal'),
   options,
 );
 
