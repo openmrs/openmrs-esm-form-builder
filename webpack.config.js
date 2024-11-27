adab8f9937d03ba689d1e3a9606c6abd71044424
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2024-11-27 11:36:51 +0530

    (refactor) Reorganize project structure and imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 const config = (module.exports = require('openmrs/default-webpack-config'));
 config.scriptRuleConfig.exclude = /(node_modules(?![/\\]@(?:openmrs|ohri)))/;
 config.overrides.resolve = {
@@ -5,6 +7,11 @@ config.overrides.resolve = {
   alias: {
     '@openmrs/esm-framework': '@openmrs/esm-framework/src/internal',
     '@openmrs/esm-form-engine-lib': '@openmrs/esm-form-engine-lib/src/index',
+    '@hooks': path.resolve(__dirname, 'src/hooks/'),
+    '@types$': path.resolve(__dirname, 'src/types.ts'),
+    '@resources': path.resolve(__dirname, 'src/resources/'),
+    '@tools': path.resolve(__dirname, 'tools/'),
+    '@constants$': path.resolve(__dirname, 'src/constants.ts'),
   },
 };
 module.exports = config;
