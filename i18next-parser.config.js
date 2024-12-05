1b489fdacce8e09e427d54761069dba69d8fa127
Ian <ian.c.bacher@gmail.com>
2024-12-05 13:42:36 -0500

    (fix) Translations should update when default value changed
    Don't know why this is necessary

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -86,4 +86,10 @@ module.exports = {
   //   message: "${defaultValue}",
   //   description: "${maxLength}", // t('my-key', {maxLength: 150})
   // }
+
+  resetDefaultValueLocale: 'en',
+  // The locale to compare with default values to determine whether a default value has been changed.
+  // If this is set and a default value differs from a translation in the specified locale, all entries
+  // for that key across locales are reset to the default value, and existing translations are moved to
+  // the `_old` file.
 };
