7a2c23e7ba15a9fb3013a0e0474268e264a98ae9
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2024-12-09 22:25:52 +0530

    Add markdown as rendering type

--- src/components/interactive-builder/modals/question/question-form/rendering-types/inputs/index.tsx
+++ src/components/interactive-builder/modals/question/question-form/rendering-types/inputs/index.tsx
@@ -4,3 +4,4 @@ export { default as Text } from './text/text.component';
 export { default as TextArea } from './text-area/textarea.component';
 export { default as Toggle } from './toggle/toggle.component';
 export { default as UiSelectExtended } from './ui-select-extended/ui-select-extended.component';
+export { default as Markdown } from './markdown/markdown.component';
