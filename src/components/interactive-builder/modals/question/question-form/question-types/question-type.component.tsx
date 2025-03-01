8dc8c61a66c07e09bb66e81a2a4c78afcd3deb1a
BharathKShetty <bks29525@gmail.com>
2025-03-02 01:25:13 +0530

    disable Save button when concept is invalid

--- src/components/interactive-builder/modals/question/question-form/question-types/question-type.component.tsx
+++ src/components/interactive-builder/modals/question/question-form/question-types/question-type.component.tsx
@@ -3,21 +3,27 @@ import { ObsTypeQuestion, ProgramStateTypeQuestion, PatientIdentifierTypeQuestio
 import { useFormField } from '../../form-field-context';
 import type { QuestionType } from '@types';
 
-const componentMap: Partial<Record<QuestionType, React.FC>> = {
+interface QuestionTypeComponentProps {
+  setIsConceptIdValid?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const componentMap: Partial<
+  Record<QuestionType, React.FC<{ setIsConceptIdValid?: React.Dispatch<React.SetStateAction<boolean>> }>>
+> = {
   obs: ObsTypeQuestion,
   programState: ProgramStateTypeQuestion,
   patientIdentifier: PatientIdentifierTypeQuestion,
   obsGroup: ObsTypeQuestion,
 };
 
-const QuestionTypeComponent: React.FC = () => {
+const QuestionTypeComponent: React.FC<QuestionTypeComponentProps> = ({ setIsConceptIdValid }) => {
   const { formField } = useFormField();
-  const Component = componentMap[formField.type];
+  const Component = componentMap[formField.type as QuestionType];
   if (!Component) {
     console.error(`No component found for questiontype: ${formField.type}`);
     return null;
   }
-  return <Component />;
+  return <Component setIsConceptIdValid={setIsConceptIdValid} />;
 };
 
 export default QuestionTypeComponent;
