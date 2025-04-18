65aca9ba98c01ab477f673c360b4a46866497f08
Vinayak Sharma <140600673+yoursanonymous@users.noreply.github.com>
2025-04-18 13:31:35 +0530

    (fix) O3-4581: Reset the rendering type when the question type changes (#426)
    * Choose a rendering type fixed for question type change 
    
    Added logic to remove rendering type value if the previously selected rendering type value is not present for the new question type
    
    * Adhered to review comments and created separate function
    
    1. Added a separate function to handle formField change
    2. Removed directly deleting line to avoid directly mutating the formField object
    
    * Review comment addressed
    
    * Reverted issue
    
    * Replaced inline function  with use callback
    
    * Change variable name
    
    ---------
    
    Co-authored-by: Nethmi Rodrigo <nethmi@openmrs.org>

--- src/components/interactive-builder/modals/question/question-form/question/question.component.tsx
+++ src/components/interactive-builder/modals/question/question-form/question/question.component.tsx
@@ -45,6 +45,35 @@ const Question: React.FC<QuestionProps> = ({ checkIfQuestionIdExists }) => {
     },
     [setFormField],
   );
+  
+  const handleQuestionTypeChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const newQuestionType = event.target.value;
+      setFormField((prevFormField) => {
+        const hasPreviousRenderingType = prevFormField?.questionOptions?.rendering;
+        if (hasPreviousRenderingType) {
+          const isQuestionTypeObs = newQuestionType === 'obs' ? true : false;
+          if (!isQuestionTypeObs) {
+            const isRenderingTypeValidForQuestionType =
+              questionTypes.includes(newQuestionType as keyof typeof renderTypeOptions) &&
+              renderTypeOptions[newQuestionType].includes(prevFormField.questionOptions.rendering as RenderType);
+            if (!isRenderingTypeValidForQuestionType) {
+              return {
+                ...prevFormField,
+                questionOptions: { ...prevFormField.questionOptions, rendering: null },
+                type: newQuestionType,
+              };
+            }
+          }
+        }
+        return {
+          ...prevFormField,
+          type: newQuestionType,
+        };
+      });
+    },
+    [setFormField],
+  );
 
   const handleQuestionInfoChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -86,9 +115,7 @@ const Question: React.FC<QuestionProps> = ({ checkIfQuestionIdExists }) => {
       />
       <Select
         value={formField?.type ?? 'control'}
-        onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
-          setFormField({ ...formField, type: event.target.value })
-        }
+        onChange={handleQuestionTypeChange}
         id="questionType"
         invalidText={t('typeRequired', 'Type is required')}
         labelText={t('questionType', 'Question type')}
